abf441ed664a50dc3f55449df05f401ac3d9c66d
august-liminal <august@liminal.build>
2025-12-03 19:06:43 +0800

    Normalise scroll

--- src/main.js
+++ src/main.js
@@ -25,7 +25,10 @@ import { ScrollTrigger } from 'https://esm.sh/gsap@3.13.0/ScrollTrigger?target=e
 import { ScrambleTextPlugin } from 'https://esm.sh/gsap@3.13.0/ScrambleTextPlugin?target=es2020&external=gsap';
 import { SplitText } from 'https://esm.sh/gsap@3.13.0/SplitText?target=es2020&external=gsap';
 gsap.registerPlugin(ScrollTrigger, ScrambleTextPlugin, SplitText);
-ScrollTrigger.normalizeScroll(true);
+ScrollTrigger.normalizeScroll({
+    allowNestedScroll: true, 
+    type: "touch,wheel"
+});
 
 const dracoLoader = new DRACOLoader();
 dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/');
